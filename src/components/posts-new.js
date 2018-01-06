42fcfecdaa32105a94e1eadd298125e70737e46f
Ankit <ankit.son.og@gmail.com>
2018-01-07 00:09:16 +0530

    wiring up /posts/new

---
+++ src/components/posts-new.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class PostsNew extends Component{
+  render(){
+    return (
+      <div>
+        NewPosts..!!
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
