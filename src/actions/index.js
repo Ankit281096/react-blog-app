c0928b1aa6c98e8921de684f94642c684990691c
Ankit <ankit.son.og@gmail.com>
2018-01-06 21:29:25 +0530

    rot route(list of posts)

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS='fetch_posts';
 
 const ROOT_URL='http://reduxblog.herokuapp.com/api';
-const API_KEY='?HELLOWORLD2810';
+const API_KEY='?key=HELLOWORLD2810';
 export function fetchPosts(){
 const request=axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
