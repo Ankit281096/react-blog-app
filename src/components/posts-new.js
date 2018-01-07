c20a09be02ae269623a90e462312d0d5c6d51c8d
Ankit <ankit.son.og@gmail.com>
2018-01-07 21:07:03 +0530

    adding route callback

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,12 +1,15 @@
 import React,{Component} from 'react';
 import {Field,reduxForm} from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component{
 
   renderField(field){
     const { meta:{ touched,error }} = field;
     const className=`form-group ${touched && error ?'has-danger' :''}`;
-    
+
     return (
       <div className={className}>
         <label>{field.label}</label>
@@ -22,7 +25,9 @@ class PostsNew extends Component{
   }
 
   onSubmit=(values)=>{
-    console.log(values);
+    this.props.createPost(values,()=>{
+      this.props.history.push('/');
+    });
   }
 
   render(){
@@ -45,6 +50,7 @@ class PostsNew extends Component{
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -67,7 +73,10 @@ function validate(values){
 
   return errors;
 }
+
 export default reduxForm({
   validate, //validate:validate
   form:'PostsNewForm'
-})(PostsNew);
+})(
+  connect (null,{createPost})(PostsNew)
+);
